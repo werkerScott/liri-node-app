9b292446e8284a7ce2077c5fbd70bb6e2578a2c9
werkerScott <scottbowerwerk@gmail.com>
2018-02-04 16:21:18 -0500

    Update keys.js

--- keys.js
+++ keys.js
@@ -1,16 +1,6 @@
 // yep i used my personal account, making this private after it is graded
 var keys = {
-	twitterKeys: {
-  		consumer_key:'Kw4bvCZNV8eDFRJ8mnAbvMC6s',
-  		consumer_secret:'Gh0gUNBbogrsdvFfdHPCb8LIjxEA8UuwiVkTJpYyCpUPdosF3U',
-  		access_token_key:'113531557-EG3N9Yz5ZObvjv8QXEeLJIvok3kc5x3BHHmwxi27',
-  		access_token_secret:'PYcZPtUyaCahr956mVkhhOpvcjpn6Mi1FqM2M9CXHCPQF'
-  	},
 
-  	spotifyKeys: {
-		client_ID: 'f5d92ad267104c349b403cfa8d8c934d',
-		client_secret: '471def40a91e4a6c86b9825bbd230147'
-	}
 
 };
 
